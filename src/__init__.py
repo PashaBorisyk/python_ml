1126b008f6e4d5db79bea3f9f7f62e45e6634be3
pborissiouk <pborissiouk@bluesoft.net.pl>
2018-11-06 16:41:52 +0100

    some data added

--- src/__init__.py
+++ src/__init__.py
@@ -1,35 +1,20 @@
 import pandas
 import math
-import matplotlib.pyplot as p
+import matplotlib.pyplot as pyplot
 import numpy
-from sklearn.svm import SVC as Model
+from sklearn.neighbors import KNeighborsClassifier
 
-data = pandas.read_csv("titanic.csv")
-data_train = data[:int(0.8*len(data))]
-data_test = data[int(0.8*len(data)):]
+mnist_data = pandas.read_csv("mnist_small.csv")
+data_train = mnist_data[:int(0.8 * len(mnist_data))]
+data_test = mnist_data[int(0.8 * len(mnist_data)):]
 
-def cat_to_num(data):
-    categories = pandas.unique(data)
-    features = {}
-    for cat in categories:
-        binary = (data == cat)
-        features["%s=%s" % (data.name, cat)] = binary.astype("int")
-    return pandas.DataFrame(features)
+features_train = data_train.drop("label", axis=1)
+features_test = data_test.drop("label",axis=1)
 
-def prepare_data(data):
-    features = data.drop(["PassengerId", "Survived", "Fare", "Name", "Sex", "Ticket", "Cabin", "Embarked"], axis=1)
-    features["Age"] = data["Age"].fillna(-1)
-    features["sqrt_Fare"] = numpy.sqrt(data["Fare"])
-    features = features.join(cat_to_num(data['Sex']))
-    features = features.join(cat_to_num(data["Embarked"]))
-    return features
+k_klassifier = KNeighborsClassifier(n_neighbors=10)
 
-features = prepare_data(data_train)
+k_klassifier.fit(features_train, data_train["label"])
+predictions = k_klassifier.predict_proba(features_test)
 
-model = Model()
-model.fit(features, data_train["Survived"])
-score = model.score(prepare_data(data_test), data_test["Survived"])
+score = k_klassifier.score(features_test,data_test["label"])
 print(score)
-
-p.plot(data_test.Fare, model.predict(prepare_data(data_test)), 'o')
-p.show()
